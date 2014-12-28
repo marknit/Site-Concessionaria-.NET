ee8de609b8537e16b122c96b040dbbf54bcedaa4
Mark Roger <mark@fiskniteroi.com.br>
2014-12-28 16:59:33 -0200

    Inclusao pagina de Pecas.aspx

--- Pecas.aspx.cs
+++ Pecas.aspx.cs
@@ -4,14 +4,36 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace NewCarMedia
 {
-    public partial class Pecas : System.Web.UI.Page
-    {
+   private static string strCon = ConfigurationManager.ConnectionStrings["CARMEDIAConnectionString"].ConnectionString;
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            //ddl.DataSource = fillddl();
+           // ddl.DataTextField = "marca";
+            //ddl.DataValueField = "marca";
+            //ddl.DataBind();
+            
+            
         }
-    }
+        public SqlDataReader fillddl()
+        {
+            string SQL = "SELECT marca FROM pecas";
+            SqlDataReader dr = null;
+            SqlConnection conn = new SqlConnection(strCon);
+
+            SqlCommand cmd = new SqlCommand(SQL, conn);
+
+            conn.Open();
+            if (conn.State == System.Data.ConnectionState.Open)
+            {
+                dr = cmd.ExecuteReader();
+
+            }
+            return dr;
+        }
+
 }
